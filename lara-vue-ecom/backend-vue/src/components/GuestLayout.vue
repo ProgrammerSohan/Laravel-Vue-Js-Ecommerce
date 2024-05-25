af6c9a9800f3285303605dd6a515e76a25ca945d
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-05-25 16:39:41 +0600

    guest-layout updated

--- lara-vue-ecom/backend-vue/src/components/GuestLayout.vue
+++ lara-vue-ecom/backend-vue/src/components/GuestLayout.vue
@@ -1,17 +1,19 @@
 <template>
-    <div>
-        <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
 
-         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{{title}}</h2>
-            </div>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
                 <slot> </slot>
 
-
           </div>
+        </div>
 
-    </div>
+
 </template>
 <script setup>
 

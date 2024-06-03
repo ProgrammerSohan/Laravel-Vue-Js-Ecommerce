3c2a6c6bd20f05dd490a66e1e74fb879653c9e68
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-06-03 16:05:50 +0600

    24b_create not found page

--- lara-vue-ecom/backend-vue/src/views/NotFound.vue
+++ lara-vue-ecom/backend-vue/src/views/NotFound.vue
@@ -1,13 +1,15 @@
 <template>
-   <div class="flex">
-    <div class="text-indigo-600">404</div>
-     <div>
-        <h1>Page Not Found</h1>
-        <p>Please check the url in address bar and try again.</p>
-        <div>
-            <button>Go back home</button>
+   <div class="flex justify-center items-center min-h-full w-full min-w-full">
+    <div class="flex">
+        <div class="text-indigo-600 text-5xl px-8 font-bold border-r pb-8 leading-10">404</div>
+        <div class="px-8">
+           <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
+           <p class="text-gray-600 mb-8">Please check the url in address bar and try again.</p>
+           <div>
+               <router-link :to="{name: 'app.dashboard'}" class="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</router-link>
+           </div>
         </div>
-     </div>
+    </div>
 
    </div>
 

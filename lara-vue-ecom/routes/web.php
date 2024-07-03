b75ef9dc4088eaebda7d17bd91d7e3e30048281f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-03 15:18:16 +0600

    61a_send email on customer registration

--- lara-vue-ecom/routes/web.php
+++ lara-vue-ecom/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', [ProductController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified' ])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

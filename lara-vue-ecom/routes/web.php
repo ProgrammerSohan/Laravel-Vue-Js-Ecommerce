bca3012a18f54cca6831a45f5488a7c940dce292
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-06 14:00:06 +0600

    69_prepare api routes for Cart Management

--- lara-vue-ecom/routes/web.php
+++ lara-vue-ecom/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,22 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function(){
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
 
+    Route::prefix('/cart')->name('cart.')->group(function(){
+        Route::get('/',[CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}',[CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}',[CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}',[CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
+
+
+});
+
+
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified' ])->name('dashboard');
@@ -26,6 +40,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 require __DIR__.'/auth.php';

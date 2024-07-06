a529dc9e0832644e235d89684744dbc289f3e76f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-06 14:36:24 +0600

    70_create middleware guestOrVerified

--- lara-vue-ecom/app/Http/Middleware/GuestOrVerified.php
+++ lara-vue-ecom/app/Http/Middleware/GuestOrVerified.php
@@ -5,16 +5,23 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Middleware\EnsureEmailIsVerified;
 
-class GuestOrVerified
+class GuestOrVerified extends EnsureEmailIsVerified
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $redirectToRoute= null): Response
     {
-        return $next($request);
+        if (!$request->user()) {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next, $redirectToRoute);
     }
 }

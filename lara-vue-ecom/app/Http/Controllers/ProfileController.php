eb5223a543c9bd7f22a2777985d14cff5c07c9c9
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-09 14:55:11 +0600

    84a_create profile request

--- lara-vue-ecom/app/Http/Controllers/ProfileController.php
+++ lara-vue-ecom/app/Http/Controllers/ProfileController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\AddressType;
+use App\Models\Country;
+use App\Models\CustomerAddress;
+
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function view(Request $request)
     {
-        return view('profile.view');
+        $user = $request->user();
+        $customer = $user->customer;
+        $shippingAddress = $customer->shippingAddress ?: new CustomerAddress(['type' => AddressType::Shipping]);
+        $billingAddress = $customer->billingAddress ?: new CustomerAddress(['type' => AddressType::Billing]);
+        dd($customer, $shippingAddress->attributesToArray(), $billingAddress, $billingAddress->customer);
+        $countries = Country::query()->orderBy('name')->get();
+        return view('profile.view',compact('customer','user','shippingAddress','billingAddress','countries'));
     }//end method
 }
--- lara-vue-ecom/app/Http/Requests/ProfileRequest.php
+++ lara-vue-ecom/app/Http/Requests/ProfileRequest.php
@@ -11,7 +11,8 @@ class ProfileRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        //return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,45 @@ class ProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'first_name' => ['required'],
+            'last_name'  => ['required'],
+            'phone' => ['required','min:7'],
+            'email' => ['required','email'],
+
+            'shipping.address1' => ['required'],
+            'shipping.address2' => ['required'],
+            'shipping.city'    => ['required'],
+            'shipping.state' => ['required'],
+            'shipping.zipcode' => ['required'],
+            'shipping.country_code' => ['required', 'exists:countries,code'],
+
+            'billing.address1' => ['required'],
+            'billing.address2' => ['required'],
+            'billing.city' => ['required'],
+            'billing.state' => ['required'],
+            'billing.zipcode' => ['required'],
+            'billing.country_code' => ['required', 'exists:countries,code'],
         ];
+    }
+
+    public function attributes()
+    {
+
+        return [
+            'billing.address1' => 'address 1',
+            'billing.address2' => 'address 2',
+            'billing.city'     => 'city',
+            'billing.state'    => 'state',
+            'billing.zipcode'  => 'zip code',
+            'billing.country_code' => 'country',
+            'shipping.address1' => 'address 1',
+            'shipping.address2' => 'address 2',
+            'shipping.city'    => 'city',
+            'shipping.state'   => 'state',
+            'shipping.zipcode' => 'zip code',
+            'shipping.country_code' => 'country',
+
+        ];
+
     }
 }
